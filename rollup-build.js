a8d2b3b18339ec9a316e9ba1df63ecb8692331d7
yarde_000 <yarden101111@gmail.com>
2017-12-26 13:50:20 +0200

    updated http client and removed request lib and uesed fetch

--- rollup-build.js
+++ rollup-build.js
@@ -9,28 +9,41 @@ const uglify = require('rollup-plugin-uglify');
 const minify = require('uglify-es').minify;
 const fsExtra = require('fs-extra');
 
-// Clean up the output directory
-// Compile source code into a distributable format with Babel
-// Copy package.json and LICENSE.txt
-Promise.resolve().then(() => del(['dist/*']))
-    .then(() => rollup.rollup({
-        entry: 'src/index.ts',
-        external: Object.keys(pkg.dependencies),
-        plugins: [
-            typescript(),
-            uglify({}, minify)
-        ],
-    }).then(bundle => bundle.write({
-        dest: `dist/index.js`,
-        format: "umd",
-        sourceMap: true,
-        moduleName: "jxCore",
-    }).then(() => {
-        delete pkg.private;
-        delete pkg.devDependencies;
-        delete pkg.scripts;
-        delete pkg.babel;
-        fsExtra.copySync('src', 'dist/src');
-        fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
-        fs.writeFileSync('dist/LICENSE', fs.readFileSync('LICENSE', 'utf-8'), 'utf-8');
-    }).catch(err => console.error(err.stack))));
+const inputOptions = {
+    input: 'src/index.ts',
+    external: Object.keys(pkg.dependencies),
+    plugins: [
+        typescript(),
+        uglify({}, minify)
+    ]
+}
+const outputOptions = {
+    file: 'dist/bundle.js',
+    format: "umd",
+    globals: {
+        "jx-injector": 'jxInjector'
+    },
+    name: "jxCore",
+    sourcemap: true
+}
+// generate code and a sourcemap
+//const { code, map } = await bundle.generate(outputOptions);
+async function build() {
+    //create new bundle
+    const bundle = await rollup.rollup(inputOptions);
+    await bundle.write(outputOptions);
+    //copy and clear unnessery code
+    onBuildDone();
+}
+
+function onBuildDone() {
+    delete pkg.private;
+    delete pkg.devDependencies;
+    delete pkg.scripts;
+    delete pkg.babel;
+    fsExtra.copySync('src', 'dist/src');
+    fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
+    fs.writeFileSync('dist/LICENSE', fs.readFileSync('LICENSE', 'utf-8'), 'utf-8');
+}
+
+build();
--- src/http-client/http-client.ts
+++ src/http-client/http-client.ts
@@ -1,57 +1,25 @@
-import * as request from 'request';
-
 export class HttpClient {
     getData(uri: string): any {
-        return new Promise((resolve: any, reject: any) => {
-            request({
-                method: "GET",
-                uri: uri
-            }).on("response", (response) => {
-                resolve(response);
-            }).on("error", (error) => {
-                reject(error);
-            });
-        });
+        return fetch(uri);
     }
 
     postData(uri: string, body: string) {
-        return new Promise((resolve: any, reject: any) => {
-            request({
-                method: "POST",
-                uri: uri,
-                body: body
-            }).on("response", (response) => {
-                resolve(response);
-            }).on("error", (error) => {
-                reject(error);
-            });
+        return fetch(uri, {
+            method: "POST",
+            body: body
         });
     }
 
     putData(uri: string, body: any) {
-        return new Promise((resolve: any, reject: any) => {
-            request({
-                method: "PUT",
-                uri: uri,
-                body: body
-            }).on("response", (response) => {
-                resolve(response);
-            }).on("error", (error) => {
-                reject(error);
-            });
+        return fetch(uri, {
+            method: "PUT",
+            body: body
         });
     }
 
     removeData(uri: string) {
-        return new Promise((resolve: any, reject: any) => {
-            request({
-                method: "DELETE",
-                uri: uri
-            }).on("response", (response) => {
-                resolve(response);
-            }).on("error", (error) => {
-                reject(error);
-            });
+        return fetch(uri, {
+            method: "DELETE",
         });
     }
 
